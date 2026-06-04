6d5a919a7b09beabe4ca98cc0974b646cfbccc94
cu2ed <42671569+jacintomfr@users.noreply.github.com>
2026-06-04 02:34:09 +0000

    Update version.h

--- version.h
+++ version.h
@@ -7,6 +7,6 @@
 //    yyy = development build (auto-incremented by ./build)
 // ══════════════════════════════════════════════════════════════
 #define LANGSTONE_VER_MAJOR  3
-#define LANGSTONE_VER_BUILD  41
-#define LANGSTONE_VERSION    "V03-041"
+#define LANGSTONE_VER_BUILD  42
+#define LANGSTONE_VERSION    "V03-042"
 #endif
